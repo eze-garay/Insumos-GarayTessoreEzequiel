c913e13d7d6a87e0ad452dbf5977ca229996478c
Ezequiel Garay <ezequielgaray37@gmail.com>
2023-01-20 22:11:13 -0300

    justificar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,26 +1,47 @@
 import'./NavBar.css'
 import LogoN from './assest/LogoN.png'
+import menuL from './assest/menu.png'
 import CartWidget from '../CartWidget/CartWidget';
 import { Link, useNavigate } from 'react-router-dom';
 import { CartContext } from '../../context/CartContext';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 
 const Navbar = () => {
-    const navigate = useNavigate ()
+
+
+const navigate = useNavigate ()
 
 const {totalQuantity} = useContext (CartContext)
 
+const [menu, setMenu] = useState ( false )
+
+const toggleMenu = () => {
+    setMenu (!menu)
+}
+
     return (
-     <nav className='Container'>
-        <img onClick={ () => navigate ('/')} src={LogoN} alt="" width="100" /> 
-      <div className='Categories'>
-          <Link to={`/category/Computadora`} className='CategoriesLink'>Computadora</Link>
-          <Link to={`/category/Notebook`} className='CategoriesLink'>Notebook</Link>
-          <Link to={`/category/Perifericos`} className='CategoriesLink'>Perifericos</Link>
-      </div>
-      <CartWidget totalQuantity={totalQuantity} />
-     </nav>
+    <header className='Container' >
+        <div className='ContainerLogo'>
+            <img className='Logo' onClick={ () => navigate ('/')} src={LogoN} alt="" />  
+        </div>
+
+    
+        <button className='ContainerButtom' onClick={toggleMenu } >
+                <img className='LogoHamb' src={menuL} alt='Hambur'></img>
+        </button>
+
+        <nav className= {`ContainerNav ${ menu ? 'isActive' : '' } `}>
+            <ul className='ContainerUl'>
+                <li className='Containerli' ><Link to={`/category/Computadora`} className='CategoriesLink'>Computadora</Link></li>
+                <li className='Containerli'><Link to={`/category/Notebook`} className='CategoriesLink'>Notebook</Link></li>
+                <li className='Containerli'><Link to={`/category/Perifericos`} className='CategoriesLink'>Perifericos</Link></li>
+                <li className='Containerli'><CartWidget totalQuantity={totalQuantity}/></li>
+            </ul>
+        </nav>
+
+
+     </header>
   
   )
 }
