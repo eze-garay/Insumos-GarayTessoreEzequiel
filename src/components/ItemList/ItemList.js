49a4bdbaaef154754a074cee7590fad1285d3244
Ezequiel Garay <ezequielgaray37@gmail.com>
2023-01-31 18:35:58 -0300

    fire y modificaciones de lista

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,6 +1,7 @@
 import Item from "../Item/Item"
 import React from "react"
 import './ItemList.css'
+import { useState } from "react"
 
 
 
@@ -11,11 +12,24 @@ import './ItemList.css'
 
 
 const ItemList = ( {products} ) => {
+
+const [list , setList] = useState (false)
+
+const lista = () =>{
+    setList(!list)
+}
+     
+
     return (
- 
-        <div className='ContainerGrilla'>
+        <div>
+        <div>
+           <button className='Button' onClick={lista }> cambiar vista </button>  
+        </div>
+        <div className= {`ContainerGrilla ${list ? 'isActive': ''} ` } > 
             {products.map(prod => <Item key={prod.id} {...prod}/>)}
         </div>
+        </div>
+        
     )
 }
 export default ItemList
